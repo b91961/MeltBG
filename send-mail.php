cba7a11174d2a014f05a66ffae1548c1657ce636
Jamal Moubarak <jamalmoubarak@fullsail.edu>
2013-09-02 15:23:28 -0400

    added smtp email settings

--- send-mail.php
+++ send-mail.php
@@ -11,10 +11,30 @@ $msg .= "EMAIL: "  .$_POST['email']    ."<br>\n";
 $msg .= "PHONE: "  .$_POST['phone']    ."<br>\n";
 $msg .= "COMMENTS: "  .$_POST['comments']    ."<br>\n";
 
+$host = "smtp.gmail.com";
+$port = "587";
+$username = "jamal@eskatech.com";
+$password = "detroit2";
+
 //Headers
 $headers  = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=UTF-8\r\n";
 $headers .= "From: <".$from. ">" ;
+
+$smtp = Mail::factory('smtp',
+   array ('host' => $host,
+     'port' => $port,
+     'auth' => true,
+     'username' => $username,
+     'password' => $password));
+ 
+$mail = $smtp->send($to, $headers, $body);
+ 
+if (PEAR::isError($mail)) {
+   echo("<p>" . $mail->getMessage() . "</p>");
+  } else {
+   echo("<p>Message successfully sent!</p>");
+  }
 
 
 //send for each mail
